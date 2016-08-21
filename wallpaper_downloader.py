14e5ea0fc985e2679f2009cadd8e2a99fb35e0ec
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2016-08-21 11:28:00 +0200

    Fixing previous commit

--- wallpaper_downloader.py
+++ wallpaper_downloader.py
@@ -2,11 +2,8 @@ import argparse
 from scraper import Scraper
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--subreddit", help="specify subreddit to scrape")
+parser.add_argument("-s", "--subreddit", help="specify subreddit to scrape", default="wallpapers")
 args = parser.parse_args()
 
-if args.subreddit:
-    scraper = Scraper(sub=args.subreddit)
-else:
-    scraper = Scraper()
+scraper = Scraper(args)
 scraper.run()
