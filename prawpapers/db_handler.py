c0a6cdba419c0671e8918967bf0504d817e88d94
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2017-06-13 11:07:56 +0200

    improve docstrings in db_handler

--- prawpapers/db_handler.py
+++ prawpapers/db_handler.py
@@ -16,25 +16,43 @@ class DbHandler:
                        "(Link TEXT, Title TEXT)")
 
     def insert_link(self, submission):
-        """Insert a link into the database"""
+        """
+        Insert a submission into the database takes a dict with:
+        {"date": "date string",
+         "url": "link-to-image"
+         "title": "title of the post"}
+        :param submission: dict in the above format
+        """
         try:
             self.c.execute("INSERT INTO downloads VALUES (?,?,?)",
                           (submission["date"],
                            submission["url"],
                            submission["title"]))
         except lite.IntegrityError:
-            # Wil happend when running with nosort since the link
-            # is in the databse
+            # Will happen when running with nosort since the link
+            # is already in the database
             pass
 
     def insert_album(self, album):
-        """Insert an album into the database"""
+        """
+        Insert an album into the database, requires a dictionary with:
+        {"url": "link-to-imgur-album",
+         "title": "post title"}
+        :param album: Dictionary
+        """
         self.c.execute("INSERT INTO albums VALUES (?,?)",
                        (album["url"],
                         album["title"]))
 
     def get_posts(self):
-        """Return all posts downloaded as a list of context dictionaries"""
+        """
+        Return all posts downloaded as a list of context dictionaries
+        in the format:
+        {"date": "date string",
+         "url": "link-to-image"
+         "title": "title of the post"}
+        :return: List of dictionaries
+        """
         self.c.execute("SELECT * FROM downloads")
         entries = self.c.fetchall()
         posts = []
@@ -47,16 +65,28 @@ class DbHandler:
         return posts
 
     def get_links(self):
-        """Return all links as a list"""
+        """
+        Return all links in the database
+        :return: list of image links
+        """
         self.c.execute("SELECT Link FROM downloads")
         return [link[0] for link in self.c.fetchall()]
 
     def get_albums_links(self):
+        """
+        Get all album links in the database
+        :return: list of imgur album links
+        """
         self.c.execute("SELECT Link FROM albums")
         return [link[0] for link in self.c.fetchall()]
 
     def sort_links(self, submissions):
-        """Sort out all previously downloaded links from a list of links"""
+        """
+        Sort out all previously downloaded links from a list of submissions
+        :param submissions: list of links to be sorted
+        :return: list of sorted submissions, 
+        and a list of discarded submissions
+        """
         new_links = []
         old_links = self.get_links()
         skipped_list = []
@@ -69,6 +99,11 @@ class DbHandler:
         return new_links, skipped_list
 
     def sort_albums(self, albums):
+        """
+        Sort out previously downloaded imgur albums from a list of imgur albums
+        :param albums: list of imgur album links to be sorted 
+        :return: list of sorted album links, and a list of discarded links
+        """
         sorted_links = []
         old_links = self.get_albums_links()
 
