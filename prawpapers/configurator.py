147a02996dddde1c4f432a22126c7b5eaabed9c0
Molex <simplesleeptimer@gmail.com>
2018-02-01 14:36:15 +0100

    Clean configurator a bit

--- prawpapers/configurator.py
+++ prawpapers/configurator.py
@@ -2,6 +2,7 @@ import configparser
 import os
 import qprompt
 
+
 class Configurator:
     def __init__(self):
         self.config = configparser.ConfigParser()
@@ -45,6 +46,10 @@ class Configurator:
             os.system('clear')
 
     def create_menu(self):
+        """
+        Creates a qprompt menu and adds all items from config
+        :return: qprompt menu
+        """
         menu = qprompt.Menu()
         for i, item in enumerate(self.config['user']):
             menu.add(str(i+1), item)
@@ -52,26 +57,34 @@ class Configurator:
         return menu
 
     def update_value(self, value):
+        """
+        Prompt the user to input a new value, then save the updated config
+        and return to menu.
+        Determines how to prompt the user by finding the value name passed as
+        argument in one of the lists in the type map.
+        :param value: value name to be updated e.g. minwidth
+        """
         desc_str = "Enter new {} (currently {})".format(value, self.config['user'][value])
         if value in self.type_map["int"]:
             self.config['user'][value] = str(qprompt.ask_int(desc_str))
         elif value in self.type_map["string"]:
             self.config['user'][value] = qprompt.ask_str(desc_str)
         elif value in self.type_map["bool"]:
+            desc_str += " yes/no"
             if qprompt.ask_yesno(desc_str):
                 self.config['user'][value] = "yes"
             else:
                 self.config['user'][value] = "no"
+
         self.save_config()
-        print('Config saved...')
         self.clear_screen()
+        print('Config saved...')
         self.menu()
 
     def menu(self):
         """Run the configurator menu allowing user to edit config"""
         menu = self.create_menu()
         selection = menu.show(returns="desc")
-        print(selection)
         if selection != "Exit":
             self.update_value(selection)
 
